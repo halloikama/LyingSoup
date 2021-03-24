a45d08ae90f24728ba30f16f711bb6350a5a068e
Rick Joosten <rpm.joosten@outlook.com>
2021-03-24 20:52:20 +0200

    finished model training

--- replay_processing/replay_preprocess.py
+++ replay_processing/replay_preprocess.py
@@ -16,160 +16,132 @@ from replay_processing.parse_replay import ReplayData
 
 
 def get_stats(replay_object):
-    diff_values = []
+    diff_values = [] 
 
-    p1_minerals = (
-        replay_object.as_dict().get("stats").get(1).get("mineral_collection_rate")
-    )
-    p2_minerals = (
-        replay_object.as_dict().get("stats").get(2).get("mineral_collection_rate")
-    )
+    p1_minerals = replay_object.as_dict().get('stats').get(1).get('mineral_collection_rate')
+    p2_minerals = replay_object.as_dict().get('stats').get(2).get('mineral_collection_rate')
 
-    p1_gas = replay_object.as_dict().get("stats").get(1).get("vespene_collection_rate")
-    p2_gas = replay_object.as_dict().get("stats").get(2).get("vespene_collection_rate")
+    p1_gas = replay_object.as_dict().get('stats').get(1).get('vespene_collection_rate')
+    p2_gas = replay_object.as_dict().get('stats').get(2).get('vespene_collection_rate')
 
-    p1_supply = replay_object.as_dict().get("stats").get(1).get("supply_consumed")
-    p2_supply = replay_object.as_dict().get("stats").get(2).get("supply_consumed")
+    p1_supply = replay_object.as_dict().get('stats').get(1).get('supply_consumed')
+    p2_supply = replay_object.as_dict().get('stats').get(2).get('supply_consumed')
 
-    p1_mineralarmy = (
-        replay_object.as_dict().get("stats").get(1).get("mineral_value_current_army")
-    )
-    p2_mineralarmy = (
-        replay_object.as_dict().get("stats").get(2).get("mineral_value_current_army")
-    )
+    p1_mineralarmy = replay_object.as_dict().get('stats').get(1).get('mineral_value_current_army')
+    p2_mineralarmy = replay_object.as_dict().get('stats').get(2).get('mineral_value_current_army')
 
-    p1_gasarmy = (
-        replay_object.as_dict().get("stats").get(1).get("vespene_value_current_army")
-    )
-    p2_gasarmy = (
-        replay_object.as_dict().get("stats").get(2).get("vespene_value_current_army")
-    )
+    p1_gasarmy = replay_object.as_dict().get('stats').get(1).get('vespene_value_current_army')
+    p2_gasarmy = replay_object.as_dict().get('stats').get(2).get('vespene_value_current_army')
 
-    p1_wsratio = replay_object.as_dict().get("stats").get(1).get("worker_supply_ratio")
-    p2_wsratio = replay_object.as_dict().get("stats").get(2).get("worker_supply_ratio")
+    p1_wsratio = replay_object.as_dict().get('stats').get(1).get('worker_supply_ratio')
+    p2_wsratio = replay_object.as_dict().get('stats').get(2).get('worker_supply_ratio')
 
-    # players do not leave the game at the same time;
-    # Truncate the data after one person has left.
     if len(p1_minerals) != len(p2_minerals):
         diff = len(p1_minerals) - len(p2_minerals)
         if diff < 0:
             p2_minerals = p2_minerals[:diff]
             p2_gas = p2_gas[:diff]
             p2_supply = p2_supply[:diff]
-            p2_mineralarmy = p2_mineralarmy[:diff]
-            p2_gasarmy = p2_gasarmy[:diff]
+            p2_mineralarmy =  p2_mineralarmy[:diff]
+            p2_gasarmy =  p2_gasarmy[:diff]
             p2_wsratio = p2_wsratio[:diff]
         else:
             p1_minerals = p1_minerals[:-diff]
             p1_gas = p1_gas[:-diff]
             p1_supply = p1_supply[:-diff]
-            p1_mineralarmy = p1_mineralarmy[:-diff]
-            p1_gasarmy = p1_gasarmy[:-diff]
+            p1_mineralarmy =  p1_mineralarmy[:-diff]
+            p1_gasarmy =  p1_gasarmy[:-diff]
             p1_wsratio = p1_wsratio[:-diff]
-
-    d_minerals = np.array(list(zip(*p1_minerals))[1]) - np.array(
-        list(zip(*p2_minerals))[1]
-    )
+    
+    d_minerals = np.array(list(zip(*p1_minerals))[1]) - np.array(list(zip(*p2_minerals))[1])
     d_gas = np.array(list(zip(*p1_gas))[1]) - np.array(list(zip(*p2_gas))[1])
     d_supply = np.array(list(zip(*p1_supply))[1]) - np.array(list(zip(*p2_supply))[1])
-    d_mineralarmy = np.array(list(zip(*p1_mineralarmy))[1]) - np.array(
-        list(zip(*p2_mineralarmy))[1]
-    )
-    d_gasarmy = np.array(list(zip(*p1_gasarmy))[1]) - np.array(
-        list(zip(*p2_gasarmy))[1]
-    )
-    # d_wsratio = np.array(list(zip(*p1_wsratio))[1]) - np.array(list(zip(*p2_wsratio))[1])
+    d_mineralarmy = np.array(list(zip(*p1_mineralarmy))[1]) - np.array(list(zip(*p2_mineralarmy))[1])
+    d_gasarmy = np.array(list(zip(*p1_gasarmy))[1]) - np.array(list(zip(*p2_gasarmy))[1])
+    d_wsratio = np.array(list(zip(*p1_wsratio))[1]) - np.array(list(zip(*p2_wsratio))[1])
+    
 
-    zipper = zip(d_supply, d_minerals, d_gas, d_mineralarmy, d_gasarmy)
-    diff_values = np.array(list(zipper))
+    zipper = zip(d_supply, d_wsratio)
+    diff_values_zipped = np.array(list(zipper))
+    data_dict_full = {"d_minerals":d_minerals.tolist(), "d_gas":d_gas.tolist(),"d_supply":d_supply.tolist(), "d_mineralarmy":d_mineralarmy.tolist(), 
+                      "d_gasarmy":d_gasarmy.tolist(),"d_wsratio":d_wsratio.tolist(), "XCV": diff_values_zipped}
+    
 
-    return diff_values
-
-
+    return diff_values_zipped, data_dict_full
+    
 def parse_input(replay_path, VERBOSE=False):
-
+    
     try:
         replay_file = replay_path
     except NameError:
-        print(
-            "\n"
-            "SETUP ERROR: Please follow the directions to add a .SC2Replay file and use\n"
-            '             "Insert to code" to set the streaming_body_1 variable to the resulting bytes.\n'
-            "             You may need to rename the data_* variable."
-        )
+        print('\n'
+              'SETUP ERROR: Please follow the directions to add a .SC2Replay file and use\n'
+              '             "Insert to code" to set the streaming_body_1 variable to the resulting bytes.\n'
+              '             You may need to rename the data_* variable.')
         raise
 
     replay = sc2reader.load_replay(
         replay_file,
-        engine=sc2reader.engine.GameEngine(
-            plugins=[ContextLoader(), APMTracker(), SelectionTracker()]
-        ),
-    )
-
+        engine=sc2reader.engine.GameEngine(plugins=[ContextLoader(), APMTracker(), SelectionTracker()]))
+    
     participants = []
     for plays in replay.players:
         participants.append(plays)
         participants.append(plays.result)
-
+    
     if VERBOSE:
         print("Replay successfully loaded.")
-
+    
         for players in replay.players:
-            print(players.result, players)
-
+            print(players.result, players )
+    
+    
     is_AI = False
     for players in replay.players:
         if isinstance(players, (sc2reader.objects.Computer)):
             is_AI = True
             if VERBOSE:
-                print("found an A.I.\n")
-
+                print('found an A.I.\n')
+                
+        
     if (len(replay.players) != 2) or (is_AI == True):
         if VERBOSE:
-            if is_AI == True:
-                print("this is an A.I. game")
+            if (is_AI == True):
+                print('this is an A.I. game')
             else:
-                print("this is not a 1v1 game")
-        return None
+                print('this is not a 1v1 game')
+        return None 
     else:
         replay_object = ReplayData.parse_replay(replay=replay)
-
+    
         try:
-            y_data = list(replay_object.as_dict().get("winners"))[0][0]
-        except:
-            y_data = 3
+            y_data = list(replay_object.as_dict().get('winners'))[0][0]
+        except: y_data = 3
+            
 
-        x_data = get_stats(replay_object)
+        x_data, data_dict_full = get_stats(replay_object)
+        data_dict_full['winner'] = y_data
 
-        data_dict = {"seq": [x_data], "winner": [y_data]}
+        data_dict = {'seq':[x_data], 'label':[y_data]}
 
-        return data_dict, participants
+        return data_dict, participants, data_dict_full, replay_object.as_dict().get('stats')
 
 
 def process_replays_from_local_folder(path_to_replays, VERBOSE=False):
     """
 
     """
+
     print("started processing replay file")
+    full_df = pd.DataFrame()
     with os.scandir(path_to_replays) as dirs:
         for entry in dirs:
             try:
-                parse, participants = parse_input(
-                    f"{path_to_replays}/{entry.name}", VERBOSE=VERBOSE
-                )
-                if "parsed_df" in locals():
-                    df_temp = pd.DataFrame(parse, index=[0])
-                    parsed_df = parsed_df.append(df_temp)
-                else:
-                    parsed_df = pd.DataFrame(parse, index=[0])
+                parse, participants, data_dict_full, replay_stats = parse_input(f"{path_to_replays}/{entry.name}", VERBOSE=VERBOSE)
+                full_df = full_df.append(data_dict_full, ignore_index=True)
             except:
-                if VERBOSE:
-                    print(f"{path_to_replays}/{entry.name}\n")
-                    print("something went wrong, parsing failed")
-                    print()
+                print(f'something went wrong, parsing of {path_to_replays}/{entry.name} failed')
+                print()
                 continue
 
-    parsed_df = parsed_df.dropna(0)
-    parsed_df.winner = parsed_df["winner"].astype("int")
-
-    return parsed_df
+    return full_df, participants, data_dict_full, parse, replay_stats
---
+++ train_model.py
@@ -0,0 +1,63 @@
+import numpy as np
+import pandas as pd
+import os
+import shutil
+import mlflow.keras
+from keras.models import Sequential, Model, load_model
+from keras.layers.core import Activation, Dropout, Dense
+from keras.layers import Flatten, LSTM, Bidirectional
+from keras.preprocessing.sequence import pad_sequences
+from keras.utils import to_categorical
+
+from replay_processing.replay_preprocess import process_replays_from_local_folder
+
+os.environ['TF_XLA_FLAGS'] = '--tf_xla_enable_xla_devices'
+MODEL_PATH ="models/soup"
+
+def train(X, Y, n_epochs, validation_split, batch_size):
+    model = Sequential()
+    model.add(LSTM(32, input_shape=(None,2), return_sequences=False, activation='relu'))
+    model.add(Dense(1, activation=('sigmoid')))
+
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+    history = model.fit(X, Y, epochs=n_epochs, validation_split=validation_split, verbose=1, batch_size=batch_size)
+    return model
+
+def train_model_from_local_files(path_to_replays, save_df=False, from_json=False, save_trained_model=True):
+    
+    if from_json:
+        train_df = pd.read_json("data/preprocessed_files.json")
+    else:
+        train_df,_, _ , _, _ = process_replays_from_local_folder(path_to_replays, VERBOSE=False)
+        #train_df = train_df.reset_index()
+
+        if save_df:
+            print("saving processed df")
+            train_df.to_json("data/preprocessed_files.json")
+    
+
+    # get categorical y-label (if player 1 wins == 1 else 0)
+    Y = train_df.winner
+    Y = (to_categorical(Y)[:,2]).astype(int)
+    Y = 1 - Y
+    train_y = np.array([[y] for y in Y])
+
+    padded_X = pad_sequences(train_df.XCV, dtype='float32', padding='pre')
+
+    trained_model = train(padded_X, train_y, n_epochs=5, validation_split=0.1, batch_size=10)
+
+    if save_trained_model:
+        if os.path.exists(MODEL_PATH):
+            shutil.rmtree(MODEL_PATH)
+        mlflow.keras.save_model(trained_model, MODEL_PATH)
+
+
+def predict(test_input):
+    model = mlflow.keras.load_model(MODEL_PATH)
+    test_output = model.predict(test_input, verbose=False)
+    return test_output
+
+if __name__ == "__main__":
+    train_model_from_local_files("data/all_data", save_df=0, from_json=0, save_trained_model=True)
+
