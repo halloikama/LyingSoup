3225ae15b7f1501e8931d4161198e3b742f4e94c
Rick Joosten <halloikama_rick@hotmail.com>
2021-05-31 12:59:10 +0300

    updated prediction pipeline

--- predict_result.py
+++ predict_result.py
@@ -2,31 +2,33 @@ import mlflow.keras
 import numpy as np
 from replay_processing.replay_preprocess import process_replays_from_local_folder
 
-def predict(test_input, model_path = "models/soup"):
-    model = mlflow.keras.load_model(model_path)
-    test_output = model.predict(test_input, verbose=False)
-    return test_output
 
-def did_the_soup_lie(prediction, players):
-    rounded_result = int(prediction.round()[0][0])
-    if (players[1] == 'Loss') and (rounded_result == 1):
+def did_the_soup_lie(path_to_replay, model):
+    (
+        full_df,
+        participants,
+        data_dict_full,
+        parse,
+        replay_stats,
+    ) = process_replays_from_local_folder(path_to_replay, VERBOSE=False)
+    test_input = np.array(parse.get("seq"))
+    pred = model.predict(test_input, verbose=False)
+
+    rounded_result = int(pred.round()[0][0])
+
+    # the supply lied if player 1 loses and the model predict it would have won
+    if (participants[1] == "Loss") and (rounded_result == 1):
         return True
     else:
         return False
 
+
 def main():
-    full_df, participants, data_dict_full, parse, replay_stats = process_replays_from_local_folder('./data/test', VERBOSE=False)
-    test_input = np.array(parse.get('seq'))
-    output = predict(test_input)
-
-    #print(test_input[0][:5])
-
-    if did_the_soup_lie(output,participants) == True:
+    if did_the_soup_lie("./data/test") == True:
         print("The soup DOES lie")
     else:
         print("The soup didn't lie")
 
-    
 
 if __name__ == "__main__":
     main()
